44b45d6aba96b49180b36452da38faaa1275fa31
karthee <karthee49@gmail.com>
2020-06-18 20:03:35 +0530

    test app flow by Roboelectrictestcase

--- testframent/src/test/java/com/karthee/testframent/AppFlowRoboElectricTest.kt
+++ testframent/src/test/java/com/karthee/testframent/AppFlowRoboElectricTest.kt
@@ -1,6 +1,10 @@
 package com.karthee.testframent
 
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
@@ -15,7 +19,32 @@ class AppFlowRoboElectricTest {
     val rule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun testHelloWorldOnTextView() {
-      //  onView(withId(R.id.hello)).check(matches(withText("Hello World!")))
+    fun testAppFlow() {
+        val firstButton = onView(withId(R.id.button_first))
+        val secondButton = onView(withId(R.id.button_second))
+        val thirdButton = onView(withId(R.id.button_third))
+        val fourthButton = onView(withId(R.id.button_fourth))
+        firstButton.check(matches(isDisplayed()))
+
+        firstButton.perform(click())
+
+        secondButton.check(matches(isDisplayed()))
+
+        secondButton.perform(click())
+
+        thirdButton.check(matches(isDisplayed()))
+
+        thirdButton.perform(click())
+
+        fourthButton.check(matches(isDisplayed()))
+
+        fourthButton.perform(click())
+
+        firstButton.check(matches(isDisplayed()))
+
+        firstButton.perform(click())
+
+
+
     }
 }
