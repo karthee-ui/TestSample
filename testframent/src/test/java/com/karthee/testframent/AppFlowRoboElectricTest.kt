f62124e02b17d89450729e6f4638fa5d11387563
karthee <karthee49@gmail.com>
2020-06-23 12:46:13 +0530

    test app flow by Roboelectrictestcase without expresso

--- testframent/src/test/java/com/karthee/testframent/AppFlowRoboElectricTest.kt
+++ testframent/src/test/java/com/karthee/testframent/AppFlowRoboElectricTest.kt
@@ -1,12 +1,10 @@
 package com.karthee.testframent
 
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.rule.ActivityTestRule
-import org.junit.Rule
+
+import android.widget.Button
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario.launch
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.runner.AndroidJUnit4
@@ -15,33 +13,70 @@ import androidx.test.runner.AndroidJUnit4
 @RunWith(AndroidJUnit4::class)
 class AppFlowRoboElectricTest {
 
-    @get:Rule
-    val rule = ActivityTestRule(MainActivity::class.java)
+//    @get:Rule
+//    val rule = Acti(MainActivity::class.java)
 
     @Test
     fun testAppFlow() {
-        val firstButton = onView(withId(R.id.button_first))
-        val secondButton = onView(withId(R.id.button_second))
-        val thirdButton = onView(withId(R.id.button_third))
-        val fourthButton = onView(withId(R.id.button_fourth))
-        firstButton.check(matches(isDisplayed()))
 
-        firstButton.perform(click())
+        // GIVEN
+        val scenario = launch(MainActivity::class.java)
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.moveToState(Lifecycle.State.RESUMED)
 
-        secondButton.check(matches(isDisplayed()))
+        scenario.onActivity {
+            activity ->
+            val buttonFirst = activity.findViewById<Button>(R.id.button_first)
 
-        secondButton.perform(click())
+            buttonFirst.callOnClick()
 
-        thirdButton.check(matches(isDisplayed()))
+            val buttonSecond
+                    = activity.findViewById<Button>(R.id.button_second)
 
-        thirdButton.perform(click())
+            assert(buttonSecond.text == "Next")
 
-        fourthButton.check(matches(isDisplayed()))
+            buttonSecond.callOnClick()
 
-        fourthButton.perform(click())
+            val buttonThird
+                    = activity.findViewById<Button>(R.id.button_third)
 
-        firstButton.check(matches(isDisplayed()))
+            assert(buttonThird.text == "Next")
 
-        firstButton.perform(click())
+            buttonThird.callOnClick()
+
+            val buttonfourth
+                    = activity.findViewById<Button>(R.id.button_fourth)
+
+            assert(buttonfourth.text == "To First")
+
+            buttonfourth.callOnClick()
+
+            assert(buttonFirst.text == "Next")
+
+
+        }
+//        val firstButton = onView(withId(R.id.button_first))
+//        val secondButton = onView(withId(R.id.button_second))
+//        val thirdButton = onView(withId(R.id.button_third))
+//        val fourthButton = onView(withId(R.id.button_fourth))
+//        firstButton.check(matches(isDisplayed()))
+//
+//        firstButton.perform(click())
+//
+//        secondButton.check(matches(isDisplayed()))
+//
+//        secondButton.perform(click())
+//
+//        thirdButton.check(matches(isDisplayed()))
+//
+//        thirdButton.perform(click())
+//
+//        fourthButton.check(matches(isDisplayed()))
+//
+//        fourthButton.perform(click())
+//
+//        firstButton.check(matches(isDisplayed()))
+//
+//        firstButton.perform(click())
     }
 }
